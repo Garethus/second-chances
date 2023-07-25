fa6d656180481a12fb337de5ebc8d81a8aeda4ac
Garethus <reyes.grethelmaec@gmail.com>
2023-07-26 08:21:42 +1000

    created products page

---
+++ client/src/pages/Products.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ProductList from "../components/ProductList";
+import Category from "../components/Category";
+import Cart from "../components/Cart";
+
+const Menu = () => {
+  return (
+    <div className="container">
+      <Category />
+      <ProductList />
+      <Cart />
+    </div>
+  );
+};
+
+export default Menu;
