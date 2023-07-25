c4b74e7f8c02598ac47e2761974e315152010e8a
Garethus <reyes.grethelmaec@gmail.com>
2023-07-26 00:06:47 +1000

    created home page and its parallax component

---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Parallax from "../components/Parallax";
+import ParallaxImg1 from "../assets/cover.jpg";
+
+
+const Home = () => {
+  return (
+    <div className="container">
+      <Parallax image={ParallaxImg1}>
+        <h1 className="main-title">Second Chances</h1>
+        <p className="subtitle">Give old phones a second chance.</p>
+        <p className="subtitle">It's good for you wallet and the environment.</p>
+      </Parallax>
+    </div>
+  );
+};
+
+export default Home;
