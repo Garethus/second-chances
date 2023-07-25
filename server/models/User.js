20f849663b598319e8b138c6bdf57e4e4b5324e3
Garethus <reyes.grethelmaec@gmail.com>
2023-07-25 20:04:24 +1000

    created user model

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const orderSchema = require('./Order');
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  orders: [orderSchema]
+});
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
